52206570fee47285d3aab2babcba71bea2922c9a
dave4506 <ffstudios1@gmail.com>
2015-09-04 22:32:18 -0700

    debuging

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,8 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
-  private def user_params
+  private
+  def user_params
     params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
   end
