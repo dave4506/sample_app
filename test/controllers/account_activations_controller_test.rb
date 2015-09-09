6824283b5c60bec17e258cc8d9d4a7f9ecbb5a0e
dave4506 <ffstudios1@gmail.com>
2015-09-09 13:19:20 -0700

    Add password resets & email confirmation

--- test/controllers/account_activations_controller_test.rb
+++ test/controllers/account_activations_controller_test.rb
@@ -5,17 +5,4 @@ class AccountActivationsControllerTest < ActionController::TestCase
   #   assert true
   # end
 
-  def edit
-    user = User.find_by(email: params[:email])
-    if user && !user.activated? && user.authenticated?(:activation, params[:id])
-      user.update_attribute(:activated,true)
-      user.update_attribute(:activated_at, Time.zone.now)
-      log_in user
-      flash[:success] = "Account activated!"
-      redirect_to user
-    else
-      flash[:danger] = "Invalid activation link"
-      redirect_to root_url
-    end
-  end
 end
