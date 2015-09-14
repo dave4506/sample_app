50f3a11a1913cf0d4211d103ee48390346fa3dd1
dave4506 <ffstudios1@gmail.com>
2015-09-13 20:17:58 -0700

    Added basic layouts

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,11 +10,20 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :password, length: { minimum: 6 }, allow_blank: true
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships, class_name:  "Relationship",
+                                    foreign_key: "follower_id",
+                                    dependent:   :destroy
+  has_many :passive_relationships, class_name:  "Relationship",
+                                    foreign_key: "followed_id",
+                                    dependent:   :destroy
+
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
 
   def feed
     Micropost.where("user_id = ?", id)
   end
-  
+
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                       BCrypt::Engine.cost
@@ -60,6 +69,20 @@ class User < ActiveRecord::Base
 
   def send_password_reset_email
     UserMailer.password_reset(self).deliver_now
+  end
+  # Follows a user.
+  def follow(other_user)
+    active_relationships.create(followed_id: other_user.id)
+  end
+
+  # Unfollows a user.
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  # Returns true if the current user is following the other user.
+  def following?(other_user)
+    following.include?(other_user)
   end
 
   private
